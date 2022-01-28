d7883859160bca15120291ff5c6241e62e14d476
Connor Moynahan <connormoynahan@julias-mbp.lan>
2022-01-27 21:55:47 -0500

    saving progress

---
+++ README.md
@@ -0,0 +1,9 @@
+IMPORTANT!! Before you run the next command make sure you don’t commit the .gitignore and vue.config.js.
+Run the command:
+`git add build && git commit -m "Initial dist subtree commit"`
+`git subtree push --prefix build origin gh-pages`
+
+## Saving these for later
+
+https://medium.com/@Roli_Dori/deploy-vue-cli-3-project-to-github-pages-ebeda0705fbd  
+https://richpauloo.github.io/2019-11-17-Linking-a-Custom-Domain-to-Github-Pages/
---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import logo from "./connor-circle.png";
+import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [factData, setFactData] = useState(null);
+  const [clockwise, setClockwise] = useState(true);
+
+  const getFacts = async () => {
+    const { data } = await axios.get(
+      "https://new-api-cm.herokuapp.com/axoltl-facts"
+    );
+    setFactData(data);
+  };
+
+  const handleImgClick = () => {
+    setClockwise(!clockwise);
+    getFacts();
+  };
+
+  useEffect(() => {
+    getFacts();
+  }, []);
+  return (
+    <div className="App-wrapper">
+      <img
+        src={logo}
+        onClick={() => handleImgClick()}
+        className={`Connor-logo ${
+          clockwise ? "clockwise" : "counter-clockwise"
+        }`}
+        alt="logo"
+      />
+      <h3>Placeholder for the Box Game PWA.</h3>
+      <h4>Here are some axoltl facts. Click my face for more.</h4>
+      {factData && factData.facts}
+      <br />
+      <br />
+      {factData && (
+        <img style={{ width: "200px" }} src={factData.url} alt="axoltl facts" />
+      )}
+    </div>
+  );
+}
+
+export default App;
